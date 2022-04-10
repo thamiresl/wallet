d8d82841f5cf93186b103d6f2e93eac87437b67c
Thamires Lourenço <thami.lourenco@outlook.com>
2022-04-10 20:41:41 -0300

    requisito 5 (incompleto)

---
+++ src/pages/Form.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="valueInput">
+            Valor:
+            <input
+              data-testid="value-input"
+              type="text"
+              name="valueInput"
+              id="valueInput"
+            />
+          </label>
+
+          <label htmlFor="descriptionInput">
+            Descrição:
+            <input
+              data-testid="description-input"
+              type="text"
+              name="descriptionInput"
+              id="descriptionInput"
+            />
+          </label>
+
+          <label htmlFor="currencyInput">
+            Moeda:
+            <select
+              data-testid="currency-input"
+              name="currencyInput"
+              id="currencyInput"
+            >
+{/*               <option>{ currency }</option> */}
+            </select>
+          </label>
+
+          <label htmlFor="payMethodInput">
+            Pagamento:
+            <select
+              data-testid="method-input"
+              name="payMethodInput"
+              id="payMethodInput"
+            >
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+
+          <label htmlFor="costInput">
+            Despesa:
+            <select
+              data-testid="tag-input"
+              name="costInput"
+              id="costInput"
+            >
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
+
+      </div>
+    );
+  }
+}
+
+export default Form;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Form from './Form';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Form />
       </div>
     );
   }
