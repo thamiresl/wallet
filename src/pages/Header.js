7c1b4a91dae67e26c9e654edd662d1aebca20d09
Thamires Lourenço <thami.lourenco@outlook.com>
2022-04-10 21:44:44 -0300

    requisito 7

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Table from './Table';
 
 class Header extends React.Component {
   render() {
     const { emailUser } = this.props;
     return (
       <div>
+        <Table />
         <p data-testid="email-field">{ emailUser }</p>
         <p data-testid="total-field">0</p>
         <p data-testid="header-currency-field">BRL</p>
